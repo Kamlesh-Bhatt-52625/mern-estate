410158371f33f4e8df7a877889c515882e1cba11
Kamlesh Bhatt <kb55262555@gmail.com>
2024-02-16 13:29:09 +0530

    Started working on the Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className='text-slate-500'>Bhatt</span>
+        <span className='text-slate-700'>Estate</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
