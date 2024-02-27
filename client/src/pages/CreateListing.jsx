51ffbcd3d87f9b54f6a562696ff163a4148e7955
Kamlesh Bhatt <kb55262555@gmail.com>
2024-02-27 12:32:18 +0530

    fix: added extra condition to show per month for rent and sell property

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -292,7 +292,9 @@ const CreateListing = () => {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>(₹ / month)</span>
+                {formData.type === "rent" && (
+                  <span className='text-xs'>(₹ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -309,7 +311,9 @@ const CreateListing = () => {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price </p>
-                  <span className='text-xs'>(₹ / month)</span>
+                  {formData.type === "rent" && (
+                    <span className='text-xs'>(₹ / month)</span>
+                  )}
                 </div>
               </div>
             )}
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -308,7 +308,9 @@ const UpdateListing = () => {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>(₹ / month)</span>
+                {formData.type === "rent" && (
+                  <span className='text-xs'>(₹ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -325,7 +327,9 @@ const UpdateListing = () => {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price </p>
-                  <span className='text-xs'>(₹ / month)</span>
+                  {formData.type === "rent" && (
+                    <span className='text-xs'>(₹ / month)</span>
+                  )}
                 </div>
               </div>
             )}
