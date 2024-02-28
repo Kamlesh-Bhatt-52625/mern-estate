33de99f892f3743f79b1353275ab978002245c76
Kamlesh Bhatt <kb55262555@gmail.com>
2024-02-28 20:15:26 +0530

    feat: Completed About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,72 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Bhatt Estate: Where Innovation Meets Showcase
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Welcome to Bhatt Estate, a virtual haven where innovation, creativity,
+        and cutting-edge technology converge to redefine the real estate
+        experience. Bhatt Estate is not your typical real estate website;
+        it&apso;s a showcase of skills, a testament to our technical prowess,
+        and a glimpse into the possibilities of the future.
+      </p>
+
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Our Unique Approach:
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        At Bhatt Estate, we&apos;ve taken a bold step to reimagine what a real
+        estate platform can be. This website is not operational for real
+        transactions; instead, it serves as a demonstration of our capabilities.
+        Every feature, every pixel, and every line of code is a showcase of the
+        skills, expertise, and passion that define Bhatt Estate.
+      </p>
+
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Functionality Beyond Boundaries:
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Explore the website to witness a symphony of functionalities. From
+        seamless property searches and advanced filters and stunning visuals,
+        Bhatt Estate is a playground of technological innovation. Our goal is to
+        push the boundaries of what&apos;s possible in the digital real estate
+        landscape.
+      </p>
+
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Connect with the Creator:
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        If you are impressed by what you see and want to explore how our skills
+        can be tailored to meet your specific needs, I encourage you to connect
+        with the creator behind Bhatt Estate. I&apos;m not just a developer;
+        I&apos;m an innovator ready to bring your digital vision to life.
+        <br />
+        You can find my contact information{" "}
+        <a
+          href='https://kamlesh-bhatt-52625.github.io/'
+          target='_blank'
+          className='text-blue-900 font-bold uppercase hover:underline'>
+          here
+        </a>
+        {". "}
+      </p>
+
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Thank you for Exploring Bhatt Estate:
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Thank you for taking the time to explore Bhatt Estate. We invite you to
+        delve into the functionalities, experience the seamless navigation, and
+        imagine the possibilities. Bhatt Estate is not just a website; it&apos;s
+        a showcase of skills, a testament to our commitment to excellence, and a
+        preview of what&apos;s achievable in the world of digital real estate.
+      </p>
+    </div>
+  );
 };
 
 export default About;
